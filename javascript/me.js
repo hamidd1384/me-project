8366f60a05f3f14ef5a885fa16ec24b29846d920
Hamid-dehmardh84 <hdhmrdh30@gmail.com>
2026-02-05 23:55:31 +0330

    my project

---
+++ javascript/me.js
@@ -0,0 +1,20 @@
+
+
+
+
+const mehead  = document.querySelector('.meheader');
+mehead.style.backgroundColor = '#3e335735';
+mehead.style.height = '100px';
+mehead.style.width = '100%';
+
+
+
+const p = document.getElementById('profil');
+p.innerHTML = "Hamid programmer";
+
+const meabout = document.getElementById('about');
+meabout.innerHTML = "about me";
+
+
+
+
